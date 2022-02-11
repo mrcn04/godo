876cdf78bdf5e1487344a214b427a38be5008514
mrcn04 <dodomrcn@gmail.com>
2022-02-11 14:21:12 +0300

    basic server conf

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Health struct {
+	Status string
+	Date   time.Time
+}
 
 func main() {
-	fmt.Println("hello godo")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", root)
+	http.HandleFunc("/health", health)
+	fmt.Println("Server started on " + port)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	health := Health{"OK", time.Now()}
+
+	js, err := json.Marshal(health)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
