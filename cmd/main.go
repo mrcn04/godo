051044d6a5c28142d0f5e80c782ae6c8763d9f41
mrcn04 <dodomrcn@gmail.com>
2022-02-17 12:40:08 +0300

    add cmd folder

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,40 @@
-package cmd
+package main
+
+import (
+	"encoding/json"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type Health struct {
+	Status string
+	Date   time.Time
+}
+
+func main() {
+	godotenv.Load("../.env")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/health", healthHandler)
+	log.Println("Server started on " + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	health := Health{"OK", time.Now()}
+
+	h, err := json.Marshal(health)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(h)
+}
